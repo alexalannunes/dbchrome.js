970394f74019ca810b0dbb0f5b43e88c85b1cd3b
AlexAlanNunes <alexalannunes@hotmail.com>
2016-10-23 10:17:13 -0200

    novos if...else

--- dbchrome.js
+++ dbchrome.js
@@ -1,36 +1,7 @@
 /* ******************************************************************************
  * Alex Alan Nunes
  * 17/07/2016 - 17:07
- * v1.3.2
- * ******************************************************************************
- * LICENSE
- * ==============================================================================
- * This is free and unencumbered software released into the public domain.
-
-  Anyone is free to copy, modify, publish, use, compile, sell, or
-  distribute this software, either in source code form or as a compiled
-  binary, for any purpose, commercial or non-commercial, and by any
-  means.
-
-  In jurisdictions that recognize copyright laws, the author or authors
-  of this software dedicate any and all copyright interest in the
-  software to the public domain. We make this dedication for the benefit
-  of the public at large and to the detriment of our heirs and
-  successors. We intend this dedication to be an overt act of
-  relinquishment in perpetuity of all present and future rights to this
-  software under copyright law.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-  OTHER DEALINGS IN THE SOFTWARE.
-	
-  For more information, please refer to <http://unlicense.org>
- * ******************************************************************************/
-
+ * v1.5.1
 
 /**
  * ini object
@@ -67,12 +38,23 @@ db.ls = {
 	 * @param key {string} nome da chave
 	 * @return {string} retorna item buscado */
 	get: function(key) {
-		return window.localStorage.getItem(key);
+		if(window.localStorage.getItem(key) === null) {
+			throw "'" + key + "' não existe";
+		}
+		else {
+			return window.localStorage.getItem(key);
+		}
 	},
 	/**
 	 * @param key {string} nome da chave */
 	remove: function(key) {
-		window.localStorage.removeItem(key);
+		if(window.localStorage.removeItem(key) === undefined) {
+			window.localStorage.removeItem(key);
+			return 1;
+		}
+		else {
+			throw "'" + key + "' não existe";
+		}
 	},
 	/**
 	 * @param key {string} nome da chave
@@ -101,12 +83,23 @@ db.ss = {
 	 * @param key {string} bome da chave
 	 * @return {string} retorna item buscado */
 	get: function(key) {
-		return window.sessionStorage.getItem(key);
+		if(window.sessionStorage.getItem(key) === null) {
+			throw "'" + key + "' não existe";
+		} 
+		else {
+			return window.sessionStorage.getItem(key);
+		}
 	},
 	/**
 	 * @param key {string} nome da chave */
 	remove: function(key) {
-		window.sessionStorage.removeItem(key);
+		if(window.sessionStorage.removeItem(key) === undefined) {
+			window.sessionStorage.removeItem(key);
+			return 1;
+		}
+		else {
+			throw "'" + key + "' não existe";
+		}
 	},
 	/**
 	 * @param key {string} nome da chave
@@ -116,7 +109,7 @@ db.ss = {
 	}
 };
 /*
- * definindo db no objeto criado $ */
+ * definindo db no objeto $ */
 window.$ = db;
 
 if($.isChrome()) {
